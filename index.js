06d6cee52677e7da7c9b6088050bcc136cb62881
Nahid-subi <scientisthassannahid@gmail.com>
2023-09-23 19:33:17 +0600

    jwt token added and jwt verify added

--- index.js
+++ index.js
@@ -10,6 +10,24 @@ app.use(cors());
 app.use(express.json())
 
 
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' })
+  }
+  // bearer token 
+  const token = authorization.split(' ')[1]
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0mmsquj.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -31,8 +49,16 @@ async function run() {
     const classesCollection = client.db("summerDb").collection("classes")
     const cartCollection = client.db("summerDb").collection("carts")
 
+    //jwt
+
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '2h' })
+      res.send({ token })
+    })
+
     //user 
-    app.get('/users',async(req,res) =>{
+    app.get('/users', async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result)
     })
@@ -48,26 +74,26 @@ async function run() {
       res.send(result)
     })
 
-    app.patch('/users/admin/:id', async(req,res)=> {
+    app.patch('/users/admin/:id', async (req, res) => {
       const id = req.params.id;
-      const filter = {_id: new ObjectId(id)};
+      const filter = { _id: new ObjectId(id) };
       const updateDoc = {
         $set: {
           role: 'admin'
         },
       };
-      const result = await usersCollection.updateOne(filter,updateDoc);
+      const result = await usersCollection.updateOne(filter, updateDoc);
       res.send(result)
     })
-    app.patch('/users/instructor/:id', async(req,res)=> {
+    app.patch('/users/instructor/:id', async (req, res) => {
       const id = req.params.id;
-      const filter = {_id: new ObjectId(id)};
+      const filter = { _id: new ObjectId(id) };
       const updateDoc = {
         $set: {
           role: 'instructor'
         },
       };
-      const result = await usersCollection.updateOne(filter,updateDoc);
+      const result = await usersCollection.updateOne(filter, updateDoc);
       res.send(result)
     })
 
@@ -89,10 +115,15 @@ async function run() {
     })
 
     //cart collection
-    app.get('/carts', async (req, res) => {
+    app.get('/carts', verifyJWT, async (req, res) => {
       const email = req.query.email;
       if (!email) {
         res.send([]);
+      }
+
+      const decodedEmail = req.decoded.email;
+      if (email !== decodedEmail) {
+        return res.status(403).send({ error: true, message: 'provident access' })
       }
       const query = { email: email };
       const result = await cartCollection.find(query).toArray();
