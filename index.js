791cda5003b1ced1c7e16ba8231aaa1935dd8f4a
Nahid-subi <scientisthassannahid@gmail.com>
2023-09-23 18:00:06 +0600

    instructor roled added

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
+const jwt = require('jsonwebtoken');
 // middleware
 app.use(cors());
 app.use(express.json())
@@ -54,6 +54,17 @@ async function run() {
       const updateDoc = {
         $set: {
           role: 'admin'
+        },
+      };
+      const result = await usersCollection.updateOne(filter,updateDoc);
+      res.send(result)
+    })
+    app.patch('/users/instructor/:id', async(req,res)=> {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updateDoc = {
+        $set: {
+          role: 'instructor'
         },
       };
       const result = await usersCollection.updateOne(filter,updateDoc);
